7eedf08731a5a6dd316e62ac82f46d955b7ca9da
Guarda <alexmaltezg@hotmail.com>
2024-08-20 00:03:41 -0600

    commit scripts db

---
+++ database-scripts/INSERTARINVENTARIO.sql
@@ -0,0 +1,71 @@
+/*INGRESO DE INVENTARIO*/
+
+/*SOBREMESA*/
+
+CALL IngresarProductoATablaProductoBase(41,'Naranja',2,0,'');
+CALL IngresarProductoATablaProductoBase(41,'Naranja',2,0,'');
+CALL IngresarProductoATablaProductoBase(41,'Naranja',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(38,'Índigo',2,0,'');
+CALL IngresarProductoATablaProductoBase(39,'Negro Transparente',6,0,'');
+CALL IngresarProductoATablaProductoBase(39,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(39,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(39,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(78,'Blanca CIB',2,0,'');
+CALL IngresarProductoATablaProductoBase(78,'Blanca',2,0,'');
+CALL IngresarProductoATablaProductoBase(86,'Negro',6,0,'Dañada, necesita tarjeta nueva');
+CALL IngresarProductoATablaProductoBase(18,'Negro',2,0,'Recap');
+
+/*PORTATILES*/
+CALL IngresarProductoATablaProductoBase(75,'Azul',2,0,'');
+CALL IngresarProductoATablaProductoBase(75,'Azul',6,0,'Pantalla superior dañada');
+CALL IngresarProductoATablaProductoBase(75,'Rojo CIB',2,0,'');
+CALL IngresarProductoATablaProductoBase(76,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(76,'Rojo',2,0,'');
+CALL IngresarProductoATablaProductoBase(76,'Navy Blue',2,0,'');
+CALL IngresarProductoATablaProductoBase(77,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(77,'Azul',6,0,'L dañado');
+CALL IngresarProductoATablaProductoBase(77,'Rojo Quemado',2,0,'');
+CALL IngresarProductoATablaProductoBase(15,'Custom',4,0,'IPS + reshell + bateria');
+CALL IngresarProductoATablaProductoBase(15,'Morado',2,0,'');
+CALL IngresarProductoATablaProductoBase(15,'Glacier',2,0,'');
+CALL IngresarProductoATablaProductoBase(15,'Glacier Reshell',2,0,'');
+CALL IngresarProductoATablaProductoBase(15,'Clear Pink',2,0,'');
+CALL IngresarProductoATablaProductoBase(15,'Gold',2,0,'Cambio Shell');
+CALL IngresarProductoATablaProductoBase(16,'Custom',6,0,'IPS + Reshell con pantalla quemada');
+CALL IngresarProductoATablaProductoBase(16,'Rojo',6,0,'Pantalla Quemada');
+CALL IngresarProductoATablaProductoBase(16,'Plateado',6,0,'Pantalla Quemada');
+CALL IngresarProductoATablaProductoBase(16,'Plateado',6,0,'Desarmado');
+CALL IngresarProductoATablaProductoBase(14,'Clear',2,0,'');
+CALL IngresarProductoATablaProductoBase(14,'Clear Purple',2,0,'');
+CALL IngresarProductoATablaProductoBase(14,'Aqua',6,0,'Falta pantalla');
+CALL IngresarProductoATablaProductoBase(14,'Morado',6,0,'Falta tapa de bateria');
+CALL IngresarProductoATablaProductoBase(14,'Amarillo',2,0,'');
+CALL IngresarProductoATablaProductoBase(12,'Clear Purple',2,0,'');
+CALL IngresarProductoATablaProductoBase(85,'Negro con Azul',6,0,'Desarmado');
+CALL IngresarProductoATablaProductoBase(85,'Negro con Azul',6,0,'Desarmado');
+CALL IngresarProductoATablaProductoBase(83,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(84,'Azul',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Rojo',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Azul',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Celeste Matte',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Blanco',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Rosa',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Negro',2,0,'');
+CALL IngresarProductoATablaProductoBase(80,'Rosa',6,0,'Dañado');
+CALL IngresarProductoATablaProductoBase(80,'Blanco',6,0,'Dañado');
+CALL IngresarProductoATablaProductoBase(80,'Negro',6,0,'Dañado');
+CALL IngresarProductoATablaProductoBase(81,'Gris',6,0,'Parte atras dañada');
+CALL IngresarProductoATablaProductoBase(81,'Blanco',2,0,'');
+CALL IngresarProductoATablaProductoBase(81,'Blanco',2,1,'');
+
+
+
+
+
+
+
---
+++ database-scripts/PROCEDIMIENTOSALMACENADOS.sql
@@ -0,0 +1,87 @@
+/* CREAR PROCEDIMIENTOS ALMACENADOS POR ROMMEL MALTEZ 28-03-2024*/
+
+use base_datos_inventario_taller;
+
+/*LISTAR PRODUCTOS EN LA TABLA PRODUCTOSBASES*/
+DELIMITER //
+CREATE PROCEDURE ListarTablaProductosBases ()
+       BEGIN
+			SELECT A.CodigoConsola, B.DescripcionConsola, A.Color, C.DescripcionEstado As 'Estado',
+				Case A.Hackeado 
+				When 0 then 'No'
+				When 1 then 'Si'
+				End as 'Hack',
+                DATE_FORMAT(A.FechaIngreso, '%d/%m/%Y') as 'Fecha Ingreso',
+                Comentario
+			FROM ProductosBases A 
+            join CatalogoConsolas B on A.Modelo = B.IdModeloConsolaPK
+            join CatalogoEstadosConsolas C on A.Estado = C.CodigoEstado;			
+       END //
+DELIMITER ;
+
+/*PROCEDIMIENTO IngresarProductoATablaProductoBase*/
+DELIMITER //
+CREATE PROCEDURE IngresarProductoATablaProductoBase (modeloP int, colorP varchar(100),EstadoP int, hackP boolean, ComentarioP varchar(100))	
+       BEGIN
+			DECLARE fecha varchar(25) default '010120';
+            DECLARE ModeloCatCons varchar(25);
+            DECLARE cantidadhoy int default 0;
+			SELECT DATE_FORMAT(NOW(), '%d%m%Y') into fecha;
+            SELECT CodigoModeloConsola from CatalogoConsolas where modelop = IdModeloConsolaPK into ModeloCatCons;
+            SELECT count(*) into cantidadhoy from ProductosBases where date(FechaIngreso)=date(date_sub(now(),interval 0 day));
+			INSERT into ProductosBases values(concat(ModeloCatCons,'-',fecha,cantidadhoy), modeloP, colorP, EstadoP, hackP, DATE_FORMAT(NOW(), '%Y%m%d'), ComentarioP);			
+       END //
+DELIMITER ;
+
+/*PROCEDIMIENTO ListarCategoriasConsolasBase*/
+DELIMITER //
+CREATE PROCEDURE ListarCategoriasConsolasBase ()
+BEGIN
+	SELECT * FROM catalogoconsolas;
+END//
+DELIMITER ;
+
+/*PROCEDIMIENTO ListarTablaProductosBasesXId modificado el dia 14 / 08 / 24*/
+/*
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ListarTablaProductosBasesXId`(IdCodigoConsola varchar(100))
+BEGIN
+			SELECT A.CodigoConsola, B.DescripcionConsola, A.Color, C.DescripcionEstado As 'Estado',
+				A.Hackeado as 'Hack',
+                DATE_FORMAT(A.FechaIngreso, '%d/%m/%Y') as 'Fecha_Ingreso',
+                Comentario
+			FROM ProductosBases A 
+            join CatalogoConsolas B on A.Modelo = B.IdModeloConsolaPK
+            join CatalogoEstadosConsolas C on A.Estado = C.CodigoEstado
+			where A.CodigoConsola = IdCodigoConsola;
+       END
+DELIMITER //
+*/
+DELIMITER //
+CREATE PROCEDURE ListarTablaProductosBasesXId(IdCodigoConsola varchar(100))
+BEGIN
+			SELECT A.CodigoConsola, B.IdModeloConsolaPK, A.Color, C.CodigoEstado,
+				A.Hackeado as 'Hack',
+                DATE_FORMAT(A.FechaIngreso, '%d/%m/%Y') as 'Fecha_Ingreso',
+                Comentario
+			FROM ProductosBases A 
+            join CatalogoConsolas B on A.Modelo = B.IdModeloConsolaPK
+            join CatalogoEstadosConsolas C on A.Estado = C.CodigoEstado
+			where A.CodigoConsola = IdCodigoConsola;
+END//
+DELIMITER ;
+
+/*PROCEDIMIENTO ActualizarProductoBase modificado el dia 19 / 08 / 24*/
+DELIMITER //
+CREATE PROCEDURE ActualizarProductoBase(IdCodigoConsola varchar(100), ModeloConsola int, ColorConsola varchar(100), EstadoConsola int, HackConsola boolean, ComentarioConsola varchar(100))
+BEGIN
+	UPDATE ProductosBases 
+    SET  
+        Modelo = ModeloConsola, 
+        Color = ColorConsola, 
+        Estado = EstadoConsola, 
+        Hackeado = HackConsola,        
+        Comentario = ComentarioConsola
+	WHERE CodigoConsola = IdCodigoConsola;
+END //
+DELIMITER ;
+
